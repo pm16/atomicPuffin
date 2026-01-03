984dcf8be473d8a0dfe52bdf025575f3babe7f3c
Comrade Puffin <comradePuffin@pm.me>
2026-01-03 20:53:15 +0700

    Removed dnf dependencies that did not resolve.

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,10 @@ set -ouex pipefail
 # this installs a package from fedora repos
 #dnf5 install -y tmux 
 dnf5 -y copr enable solopasha/hyprland
-dnf5 install -y hyprland xdg-desktop-portal-hyprland hyprland-plugins hyprpaper hypridle hyprpicker hyprlock hyprsunset hyprpolkitagent waybar uwsm hyprtile cliphist nwg-look nwg-clipman dunst pipepwire wireplumber pipewire-alsa pipewire-pulse hyprcursor hyprland-protocols hyprland-qt-support hyprland-qtutils hyprland-autoname-workspaces alacritty sddm gnome-keyring steam-devices micro tmux git curl wget distrobox podman podman-compose podman-docker qemu qemu-kvm blueman nfs-tools cfis-tools p7zip zsh yazi w3m w3m-img mpd ncmpc qt5-wayland qt6-wayland noto-fonts gparted unetbootin fastfetch bat
+dnf5 install -y hyprland xdg-desktop-portal-hyprland hyprland-plugins hyprpaper hypridle hyprpicker hyprlock hyprsunset
+dnf5 install -y hyprpolkitagent waybar uwsm wofi cliphist nwg-look nwg-clipman dunst hyprcursor hyprland-qt-support hyprland-qtutils
+dnf5 install -y hyprland-autoname-workspaces alacritty sddm gnome-keyring steam-devices micro tmux git podman-compose podman-docker qemu qemu-kvm blueman p7zip zsh
+dnf5 install -y w3m w3m-img gparted unetbootin fastfetch bat
 
 # Use a COPR Example:
 #
