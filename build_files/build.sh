b4e649a09ec42f974423faa984f09efbf3dbe1b4
Comrade Puffin <comradePuffin@pm.me>
2026-01-03 20:19:11 +0700

    Added some packages.

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+#dnf5 install -y tmux 
+dnf5 -y copr enable solopasha/hyprland
+dnf5 install -y hyprland xdg-desktop-portal-hyprland hyprland-plugins hyprpaper hypridle hyprpicker hyprlock hyprsunset hyprpolkitagent waybar uwsm hyprtile cliphist nwg-look nwg-clipman dunst pipepwire wireplumber pipewire-alsa pipewire-pulse hyprcursor hyprland-protocols hyprland-qt-support hyprland-qtutils hyprland-autoname-workspaces alacritty sddm gnome-keyring steam-devices micro tmux git curl wget distrobox podman podman-compose podman-docker qemu qemu-kvm blueman nfs-tools cfis-tools p7zip zsh yazi w3m w3m-img mpd ncmpc qt5-wayland qt6-wayland noto-fonts gparted unetbootin fastfetch bat
 
 # Use a COPR Example:
 #
